b620f24a53c3ad66b20820a73639e1e493079f7d
Melina Braconaro <melina@softtouchpos.com>
2026-02-15 10:17:28 -0300

    Add contact form

--- src/app/pages/widgets/contacto/contacto.component.ts
+++ src/app/pages/widgets/contacto/contacto.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
@@ -10,30 +10,41 @@ import { CommonModule } from '@angular/common';
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.scss']
 })
-export class ContactoComponent {
-  contactForm: FormGroup;
+export class ContactoComponent implements OnInit {
+  contactForm!: FormGroup; 
   status: 'IDLE' | 'SENDING' | 'SUCCESS' | 'ERROR' = 'IDLE';
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(
+    private fb: FormBuilder, 
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
     this.contactForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      subject: ['', [Validators.required, Validators.minLength(5)]],
-      message: ['', [Validators.required, Validators.minLength(10)]],
+      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      subject: ['', [Validators.required, Validators.minLength(3)]],
+      message: ['', [Validators.required, Validators.minLength(5)]],
     });
   }
 
   onSubmit() {
-    if (this.contactForm.invalid) return;
+    if (this.contactForm.invalid) {
+      console.log('Formulario inválido:', this.contactForm.controls);
+      return;
+    }
 
     this.status = 'SENDING';
-    
-    // Formspree endpoint (Crea tu cuenta en formspree.io y pega tu ID aquí)
-    const endpoint = 'https://formspree.io/f/tu_id_aca';
+    const endpoint = 'https://formspree.io/f/tu_id_aca'; 
 
     this.http.post(endpoint, this.contactForm.value).subscribe({
       next: () => {
         this.status = 'SUCCESS';
         this.contactForm.reset();
+        setTimeout(() => this.status = 'IDLE', 5000);
       },
       error: () => {
         this.status = 'ERROR';
